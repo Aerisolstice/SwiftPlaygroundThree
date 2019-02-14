0e83bd0348ef4670bbd391fbefcdde9244dc7e54
Aerisolstice <46410704+Aerisolstice@users.noreply.github.com>
2019-02-14 10:21:45 -1000

    Finished P6

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -5,8 +5,27 @@ import UIKit
 //print(fruitNames)
 //The above code won't work; it said that "The playground could not continue running because the playground source did not compile successfully." & "error: SwiftPlayground:3:1: error: variables currently must have an initial value when entered at the top level of the REPL var fruitnames : [String]".
 
-//Part2: square brackets = array
-var fruitNames = ["Banana", "Coconut", "Orange"]
+var spacedOut = "-------------"
+print(spacedOut)
 
-//assign fruit names
+//Part2: square brackets = array
+var fruitNames: [String] = ["Banana", "Coconut", "Orange"]
+print(fruitNames)
+
+//This is string interpolation, not iteration
+print("I like to eat\(fruitNames)")
+
+print(spacedOut)
+
+for name in fruitNames {
+    print("I like to eat " + name)
+}
+
+print(spacedOut)
+var sibNames: [String] = ["Ari", "Kas", "Lyn"]
+for item in sibNames {
+    print("I am " + item + ".")
+}
+print(spacedOut)
+
 
