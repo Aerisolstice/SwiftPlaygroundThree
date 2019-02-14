31e05bfb34934033f413ea044b2a2f61c97969e0
Aerisolstice <46410704+Aerisolstice@users.noreply.github.com>
2019-02-14 09:56:02 -1000

    Working on P2-3

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,8 +1,8 @@
 import UIKit
 
-var str = "Hello, playground"
-print (str)
-
-var fruitNames: [String]
+//var fruitNames : [String]
+//fruitNames = ["Kiwi", "Apple", "Watermelon"]
+//print(fruitNames)
+//The above code won't work; it said that "The playground could not continue running because the playground source did not compile successfully." & "error: SwiftPlayground:3:1: error: variables currently must have an initial value when entered at the top level of the REPL var fruitnames : [String]".
 
 
