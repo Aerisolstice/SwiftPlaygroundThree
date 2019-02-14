a6c0f7f03f5790c14c20132944b345e87ffbd895
Aerisolstice <46410704+Aerisolstice@users.noreply.github.com>
2019-02-14 10:30:19 -1000

    Completed P7

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -4,12 +4,13 @@ import UIKit
 //fruitNames = ["Kiwi", "Apple", "Watermelon"]
 //print(fruitNames)
 //The above code won't work; it said that "The playground could not continue running because the playground source did not compile successfully." & "error: SwiftPlayground:3:1: error: variables currently must have an initial value when entered at the top level of the REPL var fruitnames : [String]".
+//This version requires for the declaration & assignment in the same line!
 
 var spacedOut = "-------------"
 print(spacedOut)
 
 //Part2: square brackets = array
-var fruitNames: [String] = ["Banana", "Coconut", "Orange"]
+var fruitNames: [String] = ["Bananas", "Coconuts", "Oranges"]
 print(fruitNames)
 
 //This is string interpolation, not iteration
@@ -17,15 +18,25 @@ print("I like to eat\(fruitNames)")
 
 print(spacedOut)
 
-for name in fruitNames {
-    print("I like to eat " + name)
+for item in fruitNames {
+    print("I like to eat " + item + ".")
 }
 
 print(spacedOut)
 var sibNames: [String] = ["Ari", "Kas", "Lyn"]
-for item in sibNames {
-    print("I am " + item + ".")
+for name in sibNames {
+    print("I am " + name + ".")
 }
 print(spacedOut)
 
+//Part 7: intro to array indexes
+print("Between the choices of")
 
+print(fruitNames[0] + ",")
+
+print(fruitNames[1] + ",")
+
+print("and")
+print(fruitNames[2] + ";")
+
+print("We are " + fruitNames[0] + ".")
